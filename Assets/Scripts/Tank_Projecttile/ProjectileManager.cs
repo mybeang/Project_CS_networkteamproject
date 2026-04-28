a5263ad5938cfdaa6a7a44a680f702b36360db77
krokrai <mks3060@naver.com>
2026-04-28 16:45:11 +0900

    fix : 임시 Rpc 추가

--- Assets/Scripts/Tank_Projecttile/ProjectileManager.cs
+++ Assets/Scripts/Tank_Projecttile/ProjectileManager.cs
@@ -61,7 +61,8 @@ public class ProjectileManager : NetworkBehaviour
         Gizmos.DrawWireSphere(_targetPoint.point, _vechicleSO.ProjectileMaximumDamageableRange);
     }
 
-    [ServerRpc] // TODO : 서버측으로 잘 보내졌는 지 확인 필요 및 서버 부하 테스트 필요
+    //[ServerRpc] // TODO : 서버측으로 잘 보내졌는 지 확인 필요 및 서버 부하 테스트 필요
+    [Rpc(SendTo.Server, InvokePermission = RpcInvokePermission.Everyone)]
     private void DesignatDamageableGroundServerRpc(Vector3 point)
     {
         // 지정된 위치에 구형 범위를 측정 및 일정 시간(짧은 시간) 후에 범위 안에 들어간 객체(피해를 입을 수 있는 객체)들 판정( 판정할 때 조심해야될 부분이 닿은 부위를 기준으로 해야됌, 중심으로 받으면 안됌)
