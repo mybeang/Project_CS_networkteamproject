29c0593eef71f20bd49f5fe4825bcab8af85969b
Byounghee Chae <mybeang@naver.com>
2026-04-30 12:45:39 +0900

    feat: lobby room 기능 일부 구현

---
+++ Assets/Scripts/UI/LobbyRoom/LobbyRoomRoleUI.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LobbyRoomRoleUI : MonoBehaviour
+{
+    [SerializeField] private Button _moveButton;
+    [SerializeField] private TMP_InputField _userId;
+    [SerializeField] private Toggle _ready;
+    
+    [SerializeField] private int _teamNumber;
+    [SerializeField] private bool _isDriver;
+    
+    private void OnEnable() => BindCallbackToButtons();
+    private void OnDisable() => UnbindCallbackToButtons();
+
+    private void BindCallbackToButtons()
+    {
+        _moveButton.onClick.AddListener(OnMove);
+    }
+
+    private void UnbindCallbackToButtons()
+    {
+        _moveButton.onClick.RemoveListener(OnMove);
+    }
+
+    private void OnMove()
+    {
+        AssignToRole();
+    }
+
+    public bool AssignToRole()
+    {
+        if (_userId.text != "") return false;
+        _userId.text = ServiceLocator.Get<IUserInfoManager>()?.GetUserInfo().userId;
+        // UserInfo Update
+        ServiceLocator.Get<IUserInfoManager>().SetIsDriver(_isDriver);
+        ServiceLocator.Get<IUserInfoManager>().SetTeamNum(_teamNumber);
+        // Lobby Update
+        return true;
+    }
+    
+    public void UnassignFromRole()
+    {
+        _userId.text = "";
+        // UserInfo Update
+        // Lobby Update
+    }
+}
+
---
+++ Assets/Scripts/UI/LobbyRoom/LobbyRoomTeamUI.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LobbyRoomTeamUI : MonoBehaviour
+{
+    [SerializeField] private Button _moveButton;
+    [SerializeField] private LobbyRoomRoleUI _gunnerUI;
+    [SerializeField] private LobbyRoomRoleUI _driverUI;
+    
+    private void OnEnable() => BindCallbackToButtons();
+    private void OnDisable() => UnbindCallbackToButtons();
+
+    private void BindCallbackToButtons()
+    {
+        _moveButton.onClick.AddListener(OnMove);
+    }
+
+    private void UnbindCallbackToButtons()
+    {
+        _moveButton.onClick.RemoveListener(OnMove);
+    }
+
+    private void OnMove()
+    {
+        if (!_gunnerUI.AssignToRole())
+            _driverUI.AssignToRole();
+    }
+    
+}
+
