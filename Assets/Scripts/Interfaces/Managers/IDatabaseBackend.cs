cd58497a032dbedbc2914d30ffe88276171055a4
Byounghee Chae <mybeang@naver.com>
2026-05-06 11:56:53 +0900

    feat: JoinCode 관리용 DatabaseBackend 추가

--- Assets/Scripts/Interfaces/Managers/IDatabaseBackend.cs
+++ Assets/Scripts/Interfaces/Managers/IDatabaseBackend.cs
@@ -7,4 +7,7 @@ public interface IDatabaseBackend
     public void RemoveUserAsync(string userId);
     public Task<bool> ValidateDuplicateUserIdAsync(string userId);
     public void RegisterDisconnectHandler(string userId);
+    public void SetJoinCodeAsync(string roomId, string joinCode);
+    public Task<string> GetJoinCodeAsync(string roomId);
+    public void RemoveJoinCodeAsync(string roomId);
 }
